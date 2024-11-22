f010aa60e27c48fb9e3f7dc3098429a46f28178e
antoine <antoine.bochereau@fretsolutions.fr>
2024-11-22 16:13:19 +0100

    corrections

--- api/posts.js
+++ api/posts.js
@@ -6,43 +6,19 @@ let client;
 async function connectToDatabase() {
     if (!client) {
         client = new MongoClient(uri);
-        console.log(client)
         await client.connect();
     }
     return client.db();
 }
 
 export default async function handler(req, res) {
-    // Ajoute les en-têtes CORS
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-
-    // Si c'est une requête OPTIONS (pré-requête CORS), on répond immédiatement
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
-
-    if (req.method === 'POST') {
+    if (req.method === 'GET') {
         try {
             const db = await connectToDatabase();
-            const { title, body, context, firstContact, conclusion, author, readingTime } = req.body;
-
-            const newPost = {
-                title,
-                body,
-                context,
-                firstContact,
-                conclusion,
-                author,
-                createdAt: new Date(),
-                readingTime,
-            };
-
-            const result = await db.collection('posts').insertOne(newPost);
-            res.status(201).json({ message: 'Post created successfully', id: result.insertedId });
+            const posts = await db.collection('posts').find({}).toArray();
+            res.status(200).json({ data: posts });
         } catch (error) {
-            res.status(500).json({ error: 'Failed to create post' });
+            res.status(500).json({ error: 'Failed to fetch posts' });
         }
     } else {
         res.status(405).json({ error: 'Method not allowed' });
